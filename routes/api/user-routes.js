a29a991b607625c3f9c9b701af1552cce9b2760a
odupree1988 <odupree1988@gmail.com>
2021-07-14 14:13:51 -0400

    added Post model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -48,7 +48,7 @@ router.post("/", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  //expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email,
@@ -60,6 +60,7 @@ router.post("/login", (req, res) => {
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: "Incorrect password!" });
       return;
