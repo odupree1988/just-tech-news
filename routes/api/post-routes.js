dc25c73ae612593b90c925a690b9b667848a4b58
odupree1988 <odupree1988@gmail.com>
2021-07-15 16:21:08 -0400

    added comment model and endpoints

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { User, Post, Votes, Vote } = require("../../models");
+const { User, Post, Vote, Comment } = require("../../models");
 
 //get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
+    order: [["created_at", "DESC"]],
     attributes: [
       "id",
       "post_url",
@@ -18,8 +19,16 @@ router.get("/", (req, res) => {
         "vote_count",
       ],
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -84,11 +93,11 @@ router.post("/", (req, res) => {
 });
 
 //PUT /api/posts/upvote
-router.put('/upvote', (req, res) => {
+router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
   Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
